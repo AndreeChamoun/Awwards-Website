3cb86e20487312000a052b593fa43aac2c88a293
André C <35017477+AndreeChamoun@users.noreply.github.com>
2025-04-10 18:52:02 +0200

    Updating links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,14 +9,14 @@ interface FooterProps {
 const Footer: React.FC<FooterProps> = (props) => {
   const links = [
     { href: "https://discord.com", icon: <FaDiscord /> },
-    { href: "https://github.com", icon: <FaGithub /> },
+    { href: "https://github.com/AndreeChamoun", icon: <FaGithub /> },
     { href: "https:///x.com", icon: <FaXTwitter /> },
   ];
   return (
     <footer className="w-screen bg-violet-300 py-4 text-black">
       <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
         <p className="text-center text-sm md:text-left">
-          &copy; André 2025, All rights reserved
+          &copy; André Chamoun 2025, All rights reserved
         </p>
         <div className="flex justify-center gap-4 md:justify-start">
           {links.map((link) => (
@@ -25,16 +25,14 @@ const Footer: React.FC<FooterProps> = (props) => {
               href={link.href}
               target="_blank"
               rel="noopener noreferrer"
-              className="text-black transition-colors duration-500 ease-in-out hover:text-white"
-            >
+              className="text-black transition-colors duration-500 ease-in-out hover:text-white">
               {link.icon}
             </a>
           ))}
         </div>
         <a
           href="#privacy-policy"
-          className="text-conter text-sm hover:underline md:text-right"
-        >
+          className="text-conter text-sm hover:underline md:text-right">
           Privacy Policy
         </a>
       </div>
